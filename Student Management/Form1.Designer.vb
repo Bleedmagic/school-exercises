662e460e6c3b6a7bf16a1ef0ba771f789f839106
Victor Vasquez <121288976+Bleedmagic@users.noreply.github.com>
2024-10-04 15:52:07 +0800

    Add files via upload

---
+++ Student Management/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtName = New TextBox()
+        txtAge = New TextBox()
+        txtGrade = New TextBox()
+        lblStudentId = New Label()
+        lblName = New Label()
+        lblAge = New Label()
+        lblGrade = New Label()
+        btnAdd = New Button()
+        btnEdit = New Button()
+        btnDelete = New Button()
+        dgvStudents = New DataGridView()
+        lblStudentIdNum = New Label()
+        btnCancel = New Button()
+        btnSave = New Button()
+        lblTitle = New Label()
+        btnRefresh = New Button()
+        CType(dgvStudents, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(133, 131)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(140, 24)
+        txtName.TabIndex = 1
+        ' 
+        ' txtAge
+        ' 
+        txtAge.Location = New Point(133, 165)
+        txtAge.Name = "txtAge"
+        txtAge.Size = New Size(140, 24)
+        txtAge.TabIndex = 2
+        ' 
+        ' txtGrade
+        ' 
+        txtGrade.Location = New Point(133, 200)
+        txtGrade.Name = "txtGrade"
+        txtGrade.Size = New Size(140, 24)
+        txtGrade.TabIndex = 3
+        ' 
+        ' lblStudentId
+        ' 
+        lblStudentId.AutoSize = True
+        lblStudentId.Location = New Point(34, 97)
+        lblStudentId.Name = "lblStudentId"
+        lblStudentId.Size = New Size(80, 18)
+        lblStudentId.TabIndex = 4
+        lblStudentId.Text = "Student ID:"
+        ' 
+        ' lblName
+        ' 
+        lblName.AutoSize = True
+        lblName.Location = New Point(68, 131)
+        lblName.Name = "lblName"
+        lblName.Size = New Size(52, 18)
+        lblName.TabIndex = 5
+        lblName.Text = "Name:"
+        ' 
+        ' lblAge
+        ' 
+        lblAge.AutoSize = True
+        lblAge.Location = New Point(82, 168)
+        lblAge.Name = "lblAge"
+        lblAge.Size = New Size(37, 18)
+        lblAge.TabIndex = 6
+        lblAge.Text = "Age:"
+        ' 
+        ' lblGrade
+        ' 
+        lblGrade.AutoSize = True
+        lblGrade.Location = New Point(68, 200)
+        lblGrade.Name = "lblGrade"
+        lblGrade.Size = New Size(53, 18)
+        lblGrade.TabIndex = 7
+        lblGrade.Text = "Grade:"
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Cursor = Cursors.Hand
+        btnAdd.Location = New Point(364, 103)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(106, 26)
+        btnAdd.TabIndex = 8
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnEdit
+        ' 
+        btnEdit.Cursor = Cursors.Hand
+        btnEdit.Enabled = False
+        btnEdit.Location = New Point(500, 103)
+        btnEdit.Name = "btnEdit"
+        btnEdit.Size = New Size(106, 26)
+        btnEdit.TabIndex = 9
+        btnEdit.Text = "Edit"
+        btnEdit.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Cursor = Cursors.Hand
+        btnDelete.Enabled = False
+        btnDelete.Location = New Point(500, 153)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(106, 26)
+        btnDelete.TabIndex = 10
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' dgvStudents
+        ' 
+        dgvStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvStudents.Location = New Point(30, 239)
+        dgvStudents.Name = "dgvStudents"
+        dgvStudents.RowHeadersWidth = 51
+        dgvStudents.Size = New Size(576, 169)
+        dgvStudents.TabIndex = 11
+        ' 
+        ' lblStudentIdNum
+        ' 
+        lblStudentIdNum.BorderStyle = BorderStyle.Fixed3D
+        lblStudentIdNum.Location = New Point(133, 97)
+        lblStudentIdNum.Name = "lblStudentIdNum"
+        lblStudentIdNum.Size = New Size(75, 18)
+        lblStudentIdNum.TabIndex = 12
+        lblStudentIdNum.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnCancel
+        ' 
+        btnCancel.Cursor = Cursors.Hand
+        btnCancel.Enabled = False
+        btnCancel.Location = New Point(500, 200)
+        btnCancel.Name = "btnCancel"
+        btnCancel.Size = New Size(106, 26)
+        btnCancel.TabIndex = 13
+        btnCancel.Text = "Cancel"
+        btnCancel.UseVisualStyleBackColor = True
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Cursor = Cursors.Hand
+        btnSave.Enabled = False
+        btnSave.Location = New Point(364, 198)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(106, 26)
+        btnSave.TabIndex = 14
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        lblTitle.BackColor = SystemColors.ButtonFace
+        lblTitle.Font = New Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTitle.Location = New Point(34, 13)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(567, 65)
+        lblTitle.TabIndex = 15
+        lblTitle.Text = "Student Records"
+        lblTitle.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnRefresh
+        ' 
+        btnRefresh.Cursor = Cursors.Hand
+        btnRefresh.Location = New Point(364, 153)
+        btnRefresh.Name = "btnRefresh"
+        btnRefresh.Size = New Size(106, 26)
+        btnRefresh.TabIndex = 16
+        btnRefresh.Text = "Refresh"
+        btnRefresh.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 18F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        ClientSize = New Size(635, 430)
+        Controls.Add(btnRefresh)
+        Controls.Add(lblTitle)
+        Controls.Add(btnSave)
+        Controls.Add(btnCancel)
+        Controls.Add(lblStudentIdNum)
+        Controls.Add(dgvStudents)
+        Controls.Add(btnDelete)
+        Controls.Add(btnEdit)
+        Controls.Add(btnAdd)
+        Controls.Add(lblGrade)
+        Controls.Add(lblAge)
+        Controls.Add(lblName)
+        Controls.Add(lblStudentId)
+        Controls.Add(txtGrade)
+        Controls.Add(txtAge)
+        Controls.Add(txtName)
+        Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "School Form"
+        CType(dgvStudents, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents lblStudentId As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblAge As Label
+    Friend WithEvents lblGrade As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents dgvStudents As DataGridView
+    Friend WithEvents lblStudentIdNum As Label
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnRefresh As Button
+
+End Class
---
+++ Student Management/Form1.vb
@@ -0,0 +1,220 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Dim connectionString As String = "server=localhost;user=root;password=;database=student_management"
+    Dim connection As MySqlConnection
+    Dim adapter As MySqlDataAdapter
+    Dim dataTable As DataTable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Populate()
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to quit?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.No Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub Populate()
+        Try
+            connection = New MySqlConnection(connectionString)
+            connection.Open()
+
+            adapter = New MySqlDataAdapter("SELECT student_id, student_name, student_age, student_grade FROM students", connection)
+            dataTable = New DataTable()
+            adapter.Fill(dataTable)
+
+            dgvStudents.DataSource = dataTable
+
+            ResetForm()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            If connection IsNot Nothing AndAlso connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+        End Try
+    End Sub
+
+
+    Private Sub ResetForm()
+        txtName.Enabled = True
+        txtAge.Enabled = True
+        txtGrade.Enabled = True
+        btnAdd.Enabled = True
+        btnEdit.Enabled = False
+        btnDelete.Enabled = False
+        btnCancel.Enabled = False
+        btnSave.Enabled = False
+        btnRefresh.Enabled = True
+
+        txtName.Text = ""
+        txtAge.Text = ""
+        txtGrade.Text = ""
+        lblStudentIdNum.Text = ""
+
+        dgvStudents.ClearSelection()
+    End Sub
+
+
+    Private Sub dgvStudents_SelectionChanged(sender As Object, e As EventArgs) Handles dgvStudents.SelectionChanged
+        If dgvStudents.SelectedRows.Count > 0 Then
+            Dim selectedRow As DataGridViewRow = dgvStudents.SelectedRows(0)
+            lblStudentIdNum.Text = selectedRow.Cells("student_id").Value.ToString()
+            txtName.Text = selectedRow.Cells("student_name").Value.ToString()
+            txtAge.Text = selectedRow.Cells("student_age").Value.ToString()
+            txtGrade.Text = selectedRow.Cells("student_grade").Value.ToString()
+
+            txtName.Enabled = False
+            txtAge.Enabled = False
+            txtGrade.Enabled = False
+
+            btnAdd.Enabled = False
+            btnEdit.Enabled = True
+            btnDelete.Enabled = True
+            btnCancel.Enabled = True
+            btnRefresh.Enabled = False
+        Else
+            lblStudentIdNum.Text = ""
+            txtName.Text = ""
+            txtAge.Text = ""
+            txtGrade.Text = ""
+
+            txtName.Enabled = True
+            txtAge.Enabled = True
+            txtGrade.Enabled = True
+
+            btnAdd.Enabled = True
+            btnEdit.Enabled = False
+            btnDelete.Enabled = False
+            btnCancel.Enabled = False
+        End If
+    End Sub
+
+    Private Function ValidateInputs() As Boolean
+        If String.IsNullOrWhiteSpace(txtName.Text) Then
+            MessageBox.Show("Please enter a valid name.")
+            Return False
+        End If
+
+        Dim age As Integer
+        If Not Integer.TryParse(txtAge.Text, age) OrElse age < 0 Then
+            MessageBox.Show("Please enter a valid age.")
+            Return False
+        End If
+
+        Dim grade As Decimal
+        If Not Decimal.TryParse(txtGrade.Text, grade) OrElse grade < 0 OrElse grade > 100 Then
+            MessageBox.Show("Please enter a valid grade between 0 and 100.")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If ValidateInputs() Then
+            Try
+                connection = New MySqlConnection(connectionString)
+                connection.Open()
+
+                Dim query As String = "INSERT INTO students (student_name, student_age, student_grade) VALUES (@name, @age, @grade)"
+                Dim command As MySqlCommand = New MySqlCommand(query, connection)
+                command.Parameters.AddWithValue("@name", txtName.Text)
+                command.Parameters.AddWithValue("@age", txtAge.Text)
+                command.Parameters.AddWithValue("@grade", txtGrade.Text)
+                command.ExecuteNonQuery()
+
+                Populate()
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            Finally
+                If connection IsNot Nothing AndAlso connection.State = ConnectionState.Open Then
+                    connection.Close()
+                End If
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If MessageBox.Show("Are you sure you want to delete this record?", "Confirmation", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Try
+                connection = New MySqlConnection(connectionString)
+                connection.Open()
+
+                Dim query As String = "DELETE FROM students WHERE student_id = @id"
+                Dim command As MySqlCommand = New MySqlCommand(query, connection)
+                command.Parameters.AddWithValue("@id", lblStudentIdNum.Text)
+                command.ExecuteNonQuery()
+
+                Populate()
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            Finally
+                If connection IsNot Nothing AndAlso connection.State = ConnectionState.Open Then
+                    connection.Close()
+                End If
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If dgvStudents.SelectedRows.Count > 0 Then
+            txtName.Enabled = True
+            txtAge.Enabled = True
+            txtGrade.Enabled = True
+
+            btnEdit.Enabled = False
+            btnSave.Enabled = True
+            btnDelete.Enabled = False
+            btnCancel.Enabled = True
+        Else
+            MessageBox.Show("Please select a row to edit.")
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If ValidateInputs() Then
+            Dim result As DialogResult = MessageBox.Show("Are you sure you want to save changes?", "Confirmation", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                Try
+                    connection = New MySqlConnection(connectionString)
+                    connection.Open()
+
+                    Dim query As String = "UPDATE students SET student_name = @name, student_age = @age, student_grade = @grade WHERE student_id = @id"
+                    Dim cmd As MySqlCommand = New MySqlCommand(query, connection)
+                    cmd.Parameters.AddWithValue("@name", txtName.Text)
+                    cmd.Parameters.AddWithValue("@age", Convert.ToInt32(txtAge.Text))
+                    cmd.Parameters.AddWithValue("@grade", Convert.ToDecimal(txtGrade.Text))
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(lblStudentIdNum.Text))
+                    cmd.ExecuteNonQuery()
+                    ResetForm()
+                    Populate()
+                Catch ex As Exception
+                    MessageBox.Show("Error saving changes: " & ex.Message)
+                Finally
+                    connection.Close()
+                End Try
+            End If
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        ResetForm()
+    End Sub
+
+    Private Sub dgvStudents_EditingControlShowing(sender As Object, e As DataGridViewEditingControlShowingEventArgs) Handles dgvStudents.EditingControlShowing
+        If TypeOf e.Control Is TextBox Then
+            Dim textBox As TextBox = CType(e.Control, TextBox)
+            textBox.ReadOnly = True
+        End If
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Populate()
+    End Sub
+
+End Class
